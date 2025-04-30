5cf4de47a714b1a69d68a0ade96cbc79d80ea528
Alesitha-la-mejor <jeiryalejandradominguezlopez@gmail.com>
2025-04-30 13:54:08 -0700

    modifique apps

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import{useSate} from 'react'
+import './App.css'
 
-function App() {
+import AnimalsShow from './AnimalsShow'
 
-  return (
-    <>
-  <h1> Animals</h1>
-  </>
+function getRandomAnimal(){
+  const animals =['cat','bird','cow','dog','gator','horse'
+
+    return animals[Math.floor(Math.radmon()*animals.legth)]
+}
+
+function App(){
+  const[animals,setAnimals]=useState([])
+
+  const handleClick=()=>{
+    setAnimals([...animals,getRandomAnimal()])
+  }
+
+  const renderedAnimals = animals.map((animal,index)=>{
+    return <AnimalsShow type={animal} key={index}/>
+  })
+
+
+  return(
+    <div className='app'>
+      <button onClick={handleClick}>Add Animal</button>
+      <div className="animals-list">
+        {renderedAnimals}
+      </div>
+    </div>
   )
 }
 
